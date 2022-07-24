7351448ed5cc22512ccb77323c029c8d8520147f
Alexandr <voskhodov@gmail.com>
2022-07-24 09:16:25 +0500

    Решено ДЗ2

--- DZ2/Zadacha_1/zadacha1.py
+++ DZ2/Zadacha_1/zadacha1.py
@@ -3,3 +3,22 @@
 #       - 6782 -> 23
 #       - 0,56 -> 11
 
+def get_count(number):
+    s = str(number)
+    if '.' in s:
+        print (abs(s.find('.') - len(s)) - 1)
+        return abs(s.find('.') - len(s)) - 1
+    else:
+        return 0
+a = input("Введите число --> ")
+x = int(get_count(a))
+y = float(a)
+if x != 0:
+    for i in range(x):
+        y *= 10
+y = int(y)
+b = 0
+for i in range(len(a) - 1):
+    b += y % 10
+    y = y//10
+print(b)
--- DZ2/Zadacha_5/zadacha5.py
+++ DZ2/Zadacha_5/zadacha5.py
@@ -1,1 +1,5 @@
 # Реализуйте алгоритм перемешивания списка.
+list = [1, 2, 3, 4, 5]
+print(list)
+list.reverse()
+print(list)
