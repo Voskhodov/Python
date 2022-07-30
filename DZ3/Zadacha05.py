3f14bc73643dc07b34d6c13a5b02ab63a4183f4a
Alexandr <voskhodov@gmail.com>
2022-07-30 05:23:46 +0500

    Сделано ДЗ№3 добавлена задача №5

--- DZ3/Zadacha05.py
+++ DZ3/Zadacha05.py
@@ -2,7 +2,7 @@
 # Пример:
 #   - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-num_fib = int(input('Введите число  '))
+num_fib = int(input('Введите длину ряда  '))
 list_fibanachi = [0]
 x = y = 1 
 list_fibanachi.append(x)
@@ -11,7 +11,6 @@ if num_fib < 2:
     print(list_fibanachi)
 else:
     for i in range(num_fib - 2):    # (-2) уже заданы два перых числа
-    
         y, x = x, (x + y)# y приравнивается к x, x приравнивается к x + y
         list_fibanachi.append(x)
         if x > 0 and y > 0:
@@ -22,7 +21,6 @@ else:
             y*= -1
 
     list_fibanachi.reverse()
-    
     x = y = 1 
     list_fibanachi.append(x)
     list_fibanachi.append(y)
